53670fd435bc49efa57082ef18e7d39f8f860cd0
Timothy Okitsu <timothyokitsu@gmail.com>
2023-07-07 13:53:15 -0700

    Eliminated bash, random vcf files.

--- bcf_filter.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Filter input vcf
-
-FILTER="MIN(FMT/GQ) >= 105 && MIN(FMT/DP) > 10" #&& MQ > 50 && MQRankSum > -2.5 && MQRankSum < 2.5 && SOR < 3 && FS < 60"
-
-bcftools filter -O z -o $2 -i "$FILTER" $1
-
--- filtered_fmtgq99/bcf_filter.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Filter input vcf
-
-FILTER="MIN(FMT/GQ) >= 99 && MIN(FMT/DP) > 10" #&& MQ > 50 && MQRankSum > -2.5 && MQRankSum < 2.5 && SOR < 3 && FS < 60"
-
-bcftools filter -O z -o $2 -i "$FILTER" $1
-
